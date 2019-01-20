a76c0d65a330c2689e0b88cf368d4b84cea990a0
Vakoto <monko.danil@yandex.ru>
2019-01-20 19:20:15 +0500

    Стабы ядра

---
+++ GraphMaker/GraphMaker/Model/Graph.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace GraphMaker.Model
+{
+    public class Graph : IGraph
+    {
+        public INode AddNode()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteNode(INode v)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEdge AddEdge(INode v1, INode v2)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteEdge(INode v1, INode v2)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<INode> Nodes { get; }
+        public List<IEdge> Edges { get; }
+        public event GraphChangeEvent Changed;
+    }
+}
---
+++ GraphMaker/GraphMaker/Model/Node.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace GraphMaker.Model
+{
+    public class Node : INode
+    {
+        public int Number { get; }
+        public IReadOnlyList<IEdge> GetIncidentNodes { get; }
+        public IReadOnlyList<IEdge> GetIncidentEdges { get; }
+    }
+}
