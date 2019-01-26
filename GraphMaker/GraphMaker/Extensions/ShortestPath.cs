aecfdfb93fab8235c5a8d021bfe8ab2f2bf41d97
PeaceDuke <PeaceDuke@users.noreply.github.com>
2019-01-26 20:52:49 +0500

    Вроде работает, позже/завтра нормальных тестов накачу

--- GraphMaker/GraphMaker/Extensions/ShortestPath.cs
+++ GraphMaker/GraphMaker/Extensions/ShortestPath.cs
@@ -1,14 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using GraphMaker.Model;
 
 namespace GraphMaker.Extensions
 {
     public static partial class GraphExtensions
     {
+        struct PathToNode
+        {
+            public INode Node { get; set; }
+            public List<IEdge> Path { get; set; }
+            public int MinimalLenght { get; set; }
+            public bool Viseted { get; set; }
+
+            public PathToNode(INode _node, int _lenght, List<IEdge> _path)
+            {
+                Node = _node;
+                MinimalLenght = _lenght;
+                Path = _path;
+                Viseted = false;
+            }
+        }
+
         public static List<IEdge> GetShortestPath(this IGraph graph, INode start, INode end)
         {
-            throw new NotImplementedException();
+            List<PathToNode> nodePaths = new List<PathToNode>() { new PathToNode(start, 0, new List<IEdge>()) };
+            for(int i = 0; i < nodePaths.Count; i++)
+            {
+                nodePaths[i].Node.IncidentEdges.Sort((x, y) => x.Length.CompareTo(y.Length));
+                foreach (var incidentEdge in nodePaths[i].Node.IncidentEdges)
+                {
+                    var secondNodePathIndex = nodePaths.FindIndex(x => x.Node == incidentEdge.OtherNode(nodePaths[i].Node));
+                    if (secondNodePathIndex < 0)
+                    {
+                        var newPath = new List<IEdge>(nodePaths[i].Path);
+                        newPath.Add(incidentEdge);
+                        nodePaths.Add(new PathToNode(incidentEdge.OtherNode(nodePaths[i].Node), 
+                            nodePaths[i].MinimalLenght + incidentEdge.Length, newPath));
+                    }
+                    else if(!nodePaths[secondNodePathIndex].Viseted)
+                    {
+                        if (nodePaths[i].MinimalLenght + incidentEdge.Length < nodePaths[secondNodePathIndex].MinimalLenght)
+                        {
+                            var newPathToNode = nodePaths[i];
+                            newPathToNode.MinimalLenght = nodePaths[i].MinimalLenght + incidentEdge.Length;
+                            newPathToNode.Path.Add(incidentEdge);
+                            newPathToNode.Node = nodePaths[secondNodePathIndex].Node;
+                            nodePaths[secondNodePathIndex] = newPathToNode;
+                        }
+                    }
+                }
+                var t = nodePaths[i];
+                t.Viseted = true;
+                nodePaths[nodePaths.FindIndex(x => x.Equals(nodePaths[i]))] = t;
+            }
+            return nodePaths.Find(x => x.Node == end && x.Viseted == true).Path;
         }
     }
 }
---
+++ GraphMaker/GraphMaker/Tests/ShortestPathTest.cs
@@ -0,0 +1,47 @@
+﻿using GraphMaker.Model;
+using NUnit.Framework;
+using GraphMaker.Extensions;
+
+namespace GraphMaker.Tests
+{
+    [TestFixture]
+    public class ShortestPathTest
+    {
+
+        private IGraph MakeGraph(int nodesCount, params (int First, int Second, int Length)[] incidentEdges)
+        {
+            IGraph graph = new Graph();
+
+            for (var i = 0; i < nodesCount; i++)
+            {
+                graph.AddNode();
+            }
+
+            var nodes = graph.Nodes;
+            foreach (var edge in incidentEdges)
+            {
+                var first = nodes[edge.First];
+                var second = nodes[edge.Second];
+                graph.AddEdge(first, second, edge.Length);
+            }
+
+            return graph;
+        }
+
+        [Test, TestCaseSource(typeof(TestsFactory), nameof(TestsFactory.TestCases))]
+        public void Test(int nodesCount, (int First, int Second, int Length)[] incidentEdges, int expectedCount)
+        {
+            var graph = MakeGraph(nodesCount, incidentEdges);
+
+            graph.GetShortestPath(graph.Nodes[0], graph.Nodes[4]);
+        }
+
+        private class TestsFactory
+        {
+            public static TestCaseData[] TestCases => new[]
+            {
+                new TestCaseData(5, new[] {(0, 1, 3), (0, 2, 1), (0, 3, 4), (1, 2, 1), (1, 4, 1), (2, 3, 6), (3, 4, 5)}, 1),
+            };
+        }
+    }
+}
